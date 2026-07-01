e00dc25621c07d133319473ebfcd6df45a1e491d
jameela <jameelahabib12@gmail.com>
2026-07-01 11:35:07 +0500

    changed the config

--- config/db.js
+++ config/db.js
@@ -1,30 +1,21 @@
 ﻿const mongoose = require("mongoose");
-require("dotenv").config();
 
-let cached = global.mongoose;
+let isConnected = false;
 
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
-const connectDB = async () => {
-    const MONGODB_URI = process.env.MONGO_URI;
-
-    if (!MONGODB_URI) {
-        throw new Error("MONGO_URI environment variable is not defined");
+async function connectDB() {
+    if (isConnected) {
+        return;
     }
 
-    if (cached.conn) {
-        return cached.conn;
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is missing");
     }
 
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI).then((mongooseInstance) => mongooseInstance.connection);
-    }
+    await mongoose.connect(process.env.MONGO_URI);
 
-    cached.conn = await cached.promise;
+    isConnected = true;
+
     console.log("✅ MongoDB Connected");
-    return cached.conn;
-};
+}
 
 module.exports = connectDB;
